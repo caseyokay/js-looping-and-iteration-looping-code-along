9e61e16b6571e88432b120f28d6e9cf3ad89cc45
github username <github email address>
2020-10-28 22:03:26 -0400

    Done

--- index.js
+++ index.js
@@ -32,10 +32,3 @@ function countDown(number){
 
 
   
-//   function countDown( startingNumber ) {
-//     while ( startingNumber > 0 ) {
-//       console.log( startingNumber );
-//       startingNumber -= 1;
-//     }
-//     console.log( startingNumber );
-//   }
